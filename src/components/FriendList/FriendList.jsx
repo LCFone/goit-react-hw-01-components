4ea60e6f043e0f7e4e216d9923979e32082ae2c0
LCFone <putilov.k@gmail.com>
2023-09-26 21:06:58 +0300

    1hw-react

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import css from './FriendList.module.css';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css['friend-list']}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id}
+        />
+      ))}
+    </ul>
+  );
+};
